8cd68357245ac34e11e6a3d6a5dfe1dee3ba8ad6
grubalcava <70352823+grubalcava@users.noreply.github.com>
2020-09-28 21:51:12 -0700

    Update Morty.hpp

--- src/Morty.hpp
+++ src/Morty.hpp
@@ -34,7 +34,21 @@ namespace interdimensional {
 //          Declare Namespace C137 and enclosed functions here 
 // =========================================================================== // 
 
+namespace C137 {
+	void Morty(int start, int stop);
 
+	void Morty(int start, int stop, int step);
+
+}
+
+
+namespace Z286 {
+
+	void Morty(int start, int stop);
+
+	void Morty(int start, int stop, int step);
+
+}
 
 // =========================================================================== // 
 //          Declare Namespace Z286 and enclosed functions here 
